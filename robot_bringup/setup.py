60214fd87ebbf20a33f83ec72018230ad69cc0d8
dhruvil122 <dhruvilmahidhariya@gmail.com>
2025-05-18 03:26:01 +0200

    camera script added

--- robot_bringup/setup.py
+++ robot_bringup/setup.py
@@ -24,6 +24,7 @@ setup(
             'hand_tracking = robot_bringup.hand_tracking:main',
             'teleop_moveit = robot_bringup.teleop_moveit:main',
             'test_ik = robot_bringup.test_ik:main',
+
         ],
     },
 )
